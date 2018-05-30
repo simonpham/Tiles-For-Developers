82d4f21595c6df5fecab0750a99b806183b0fe20
Simon Pham <simonpham.dn@gmail.com>
2018-05-30 20:19:20 +0700

    Initialize Demo Mode tile

--- app/src/main/java/com/github/simonpham/tiles4devs/tiles/DemoModeService.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/tiles/DemoModeService.kt
@@ -1,12 +1,25 @@
 package com.github.simonpham.tiles4devs.tiles
 
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
+import com.github.simonpham.tiles4devs.isDemoModeEnabled
 
 /**
  * Created by Simon Pham on 5/30/18.
  * Email: simonpham.dn@gmail.com
  */
 
-class DemoModeService: TileService() {
-    
+class DemoModeService : TileService() {
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+
+        if (isDemoModeEnabled()) {
+            qsTile.state = Tile.STATE_ACTIVE
+        } else {
+            qsTile.state = Tile.STATE_INACTIVE
+        }
+
+        qsTile.updateTile()
+    }
 }
