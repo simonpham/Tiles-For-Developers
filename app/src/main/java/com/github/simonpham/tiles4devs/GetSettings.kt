21e369eb78b8216669a38c16657d3c9f61594899
Simon Pham <simonpham.dn@gmail.com>
2018-05-30 20:18:23 +0700

    Create GetSettings class

---
+++ app/src/main/java/com/github/simonpham/tiles4devs/GetSettings.kt
@@ -0,0 +1,11 @@
+package com.github.simonpham.tiles4devs
+
+/**
+ * Created by Simon Pham on 5/30/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+fun isDemoModeEnabled(): Boolean {
+    // TODO: implement method
+    return false
+}
