df8a56f5f3b85a955c46ffaa462c5665d28e7601
Simon Pham <simonpham.dn@gmail.com>
2018-05-30 19:47:07 +0700

    Create Demo mode service

---
+++ app/src/main/java/com/github/simonpham/tiles4devs/tiles/DemoModeService.kt
@@ -0,0 +1,12 @@
+package com.github.simonpham.tiles4devs.tiles
+
+import android.service.quicksettings.TileService
+
+/**
+ * Created by Simon Pham on 5/30/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+class DemoModeService: TileService() {
+    
+}
