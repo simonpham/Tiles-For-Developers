6ba184697e193ead2856723a84c27de3d7ef45bb
Simon Pham <simonpham.dn@gmail.com>
2018-05-31 21:49:04 +0700

    Add some system property constants

--- app/src/main/java/com/github/simonpham/tiles4devs/Const.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/Const.kt
@@ -5,4 +5,8 @@ package com.github.simonpham.tiles4devs
  * Email: simonpham.dn@gmail.com
  */
 
-const val SYSPROPS_TRANSACTION = 1599295570
+const val SYSPROP_DEBUG_FORCE_RTL = "debug.force_rtl"
+const val SYSPROP_DEBUG_GPU_OVERDRAW = "debug.hwui.overdraw"
+const val SYSPROP_DEBUG_GPU_PROFILE = "debug.hwui.profile"
+const val SYSPROP_DEBUG_LAYOUT = "debug.layout"
+const val SYSPROP_SYSUI_DEMO_ALLOWED = "sysui_demo_allowed"
