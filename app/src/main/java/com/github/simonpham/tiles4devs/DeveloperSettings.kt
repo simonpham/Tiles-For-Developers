e7c1d87d36a1aa869c9d88812e6a1403e624e3f3
Simon Pham <simonpham.dn@gmail.com>
2018-06-01 00:23:24 +0700

    Add DeveloperSettings class

---
+++ app/src/main/java/com/github/simonpham/tiles4devs/DeveloperSettings.kt
@@ -0,0 +1,10 @@
+package com.github.simonpham.tiles4devs
+
+/**
+ * Created by Simon Pham on 5/31/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+fun kickSystemService() {
+    SystemPropPoker().execute()
+}
