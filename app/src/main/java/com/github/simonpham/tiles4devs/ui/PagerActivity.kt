5846f63fb30752af9f48969d224c22bdd9bd9c3b
Simon Pham <simonpham.dn@gmail.com>
2018-06-01 02:10:20 +0700

    Init onboard screen

--- app/src/main/java/com/github/simonpham/tiles4devs/ui/PagerActivity.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/ui/PagerActivity.kt
@@ -1,69 +1,89 @@
 package com.github.simonpham.tiles4devs.ui
 
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-
+import android.animation.ArgbEvaluator
+import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
-import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.ImageView
 import com.github.simonpham.tiles4devs.R
+import com.github.simonpham.tiles4devs.SingletonInstances
 import kotlinx.android.synthetic.main.activity_pager.*
 import kotlinx.android.synthetic.main.fragment_pager.view.*
 
+
 class PagerActivity : AppCompatActivity() {
 
-    /**
-     * The [android.support.v4.view.PagerAdapter] that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * [android.support.v4.app.FragmentStatePagerAdapter].
-     */
+    private val context = SingletonInstances.getAppContext()
     private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    var color1 = ContextCompat.getColor(context, R.color.lightBlue)
+    var color2 = ContextCompat.getColor(context, R.color.orange)
+    var color3 = ContextCompat.getColor(context, R.color.green)
+    var color4 = ContextCompat.getColor(context, R.color.red)
+    var colorList = intArrayOf(color1, color2, color3, color4)
+
+    private var indicators: Array<ImageView>? = null
+    var page = 0   //  to track page position
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pager)
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        viewPager.adapter = mSectionsPagerAdapter
 
-        // Set up the ViewPager with the sections adapter.
-        container.adapter = mSectionsPagerAdapter
+        indicators = arrayOf(intro_indicator_0, intro_indicator_1, intro_indicator_2, intro_indicator_3)
 
+        viewPager.addOnPageChangeListener((object : ViewPager.OnPageChangeListener {
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                val evaluator = ArgbEvaluator()
+                val colorUpdate = evaluator.evaluate(positionOffset, colorList[position], colorList[if (position == 3) position else position + 1]) as Int
+                viewPager.setBackgroundColor(colorUpdate)
+            }
+
+            override fun onPageSelected(position: Int) {
+                page = position
+                updateIndicators(page)
+
+                when (position) {
+                    0 -> viewPager.setBackgroundColor(color1)
+                    1 -> viewPager.setBackgroundColor(color2)
+                    2 -> viewPager.setBackgroundColor(color3)
+                    3 -> viewPager.setBackgroundColor(color4)
+                }
+            }
+        }))
     }
 
-    /**
-     * A [FragmentPagerAdapter] that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
+    fun updateIndicators(position: Int) {
+        for (i in 0 until indicators!!.size) {
+            indicators!![i].setBackgroundResource(
+                    if (i == position) R.drawable.indicator_selected else R.drawable.indicator_unselected
+            )
+        }
+    }
+
     inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
         override fun getItem(position: Int): Fragment {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1)
         }
 
-        override fun getCount(): Int {
-            // Show 3 total pages.
-            return 3
-        }
+        override fun getCount(): Int = 4
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     class PlaceholderFragment : Fragment() {
 
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -74,16 +94,8 @@ class PagerActivity : AppCompatActivity() {
         }
 
         companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
             private val ARG_SECTION_NUMBER = "section_number"
 
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
             fun newInstance(sectionNumber: Int): PlaceholderFragment {
                 val fragment = PlaceholderFragment()
                 val args = Bundle()
