c35664b20aee46653947068591bed037c23665f8
Simon Pham <simonpham.dn@gmail.com>
2018-05-31 22:26:49 +0700

    Add Global Int getter/setter method

--- app/src/main/java/com/github/simonpham/tiles4devs/service/BaseTileService.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/service/BaseTileService.kt
@@ -1,5 +1,6 @@
 package com.github.simonpham.tiles4devs.service
 
+import android.provider.Settings
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import com.github.simonpham.tiles4devs.R
@@ -26,5 +27,24 @@ abstract class BaseTileService : TileService() {
 
     protected fun showPermissionError() {
         toast(R.string.toast_permission_required)
+    }
+
+    protected fun setGlobalInt(key: String, value: Int) {
+        try {
+            Settings.Global.putInt(contentResolver, key, value)
+        } catch (se: SecurityException) {
+            showPermissionError()
+        }
+    }
+
+    protected fun getGlobalInt(key: String): Int {
+        var value = 0
+        try {
+            value = Settings.Global.getInt(contentResolver, key)
+        } catch (se: SecurityException) {
+            showPermissionError()
+        } finally {
+            return value
+        }
     }
 }
