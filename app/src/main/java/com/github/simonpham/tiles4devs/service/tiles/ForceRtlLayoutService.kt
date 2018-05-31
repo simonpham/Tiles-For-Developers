860395297a7579b6e7bcd304dcf9802bea99aabb
Simon Pham <simonpham.dn@gmail.com>
2018-06-01 00:25:08 +0700

    Update ForceRtlLayoutService class

--- app/src/main/java/com/github/simonpham/tiles4devs/service/tiles/ForceRtlLayoutService.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/service/tiles/ForceRtlLayoutService.kt
@@ -1,8 +1,11 @@
 package com.github.simonpham.tiles4devs.service.tiles
 
-import android.provider.Settings
+import android.os.SystemProperties
+import android.service.quicksettings.Tile
 import com.github.simonpham.tiles4devs.SYSPROP_DEBUG_FORCE_RTL
+import com.github.simonpham.tiles4devs.kickSystemService
 import com.github.simonpham.tiles4devs.service.BaseTileService
+
 
 /**
  * Created by Simon Pham on 5/30/18.
@@ -10,24 +13,17 @@ import com.github.simonpham.tiles4devs.service.BaseTileService
  */
 
 class ForceRtlLayoutService : BaseTileService() {
-
-    override fun onClick() {
-        val newValue = if (isFeatureEnabled()) 0 else 1
-
-        try {
-            setGlobalInt(SYSPROP_DEBUG_FORCE_RTL, newValue)
-        } catch (e: Exception) {
-            showPermissionError()
-        }
-
-        refresh()
+    override fun refresh() {
+        val enabled = SystemProperties.getInt(SYSPROP_DEBUG_FORCE_RTL, 0) == 1
+        qsTile.state = if (enabled) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
     }
 
-    override fun isFeatureEnabled(): Boolean {
-        return try {
-            getGlobalInt(SYSPROP_DEBUG_FORCE_RTL) == 1
-        } catch (e: Settings.SettingNotFoundException) {
-            false
-        }
+
+    override fun onClick() {
+        SystemProperties.set(SYSPROP_DEBUG_FORCE_RTL,
+                if (qsTile.state == Tile.STATE_INACTIVE) "1" else "0")
+        kickSystemService() // Settings app magic
+        refresh()
     }
 }
