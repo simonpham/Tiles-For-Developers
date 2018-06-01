ac8bd1beafb4dc511b938c8ffb2309fac33716fa
Simon Pham <simonpham.dn@gmail.com>
2018-06-01 20:51:19 +0700

    Add set view visibility util

--- app/src/main/java/com/github/simonpham/tiles4devs/util/ViewUtils.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/util/ViewUtils.kt
@@ -2,12 +2,25 @@ package com.github.simonpham.tiles4devs.util
 
 import android.content.Context
 import android.support.annotation.StringRes
+import android.view.View
 import android.widget.Toast
 
 /**
  * Created by Simon Pham on 5/31/18.
  * Email: simonpham.dn@gmail.com
  */
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
 
 private fun showToast(context: Context, message: String?, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(context, message ?: "null", duration).show()
