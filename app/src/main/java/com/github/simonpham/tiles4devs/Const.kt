c8516564c8d08a1f5ecf13907cc7d3147548c0d0
Simon Pham <simonpham.dn@gmail.com>
2018-06-01 14:21:18 +0700

    Add some viewpager constants

--- app/src/main/java/com/github/simonpham/tiles4devs/Const.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/Const.kt
@@ -11,3 +11,9 @@ const val SYSPROP_DEBUG_GPU_PROFILE = "debug.hwui.profile"
 const val SYSPROP_DEBUG_LAYOUT = "debug.layout"
 const val SYSPROP_DEMO_MODE_ALLOWED = "sysui_demo_allowed"
 const val SYSPROP_DEMO_MODE_ON = "sysui_tuner_demo_on"
+
+const val PAGE_COUNT = 4
+const val PAGE_WELCOME = 0
+const val PAGE_REQUEST_PERMISSION = 1
+const val PAGE_ADD_TILE_GUIDE = 2
+const val PAGE_START_DEVELOPING = 3
--- app/src/main/java/com/github/simonpham/tiles4devs/ui/ViewPagerAdapter.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/ui/ViewPagerAdapter.kt
@@ -3,6 +3,8 @@ package com.github.simonpham.tiles4devs.ui
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import com.github.simonpham.tiles4devs.*
+import com.github.simonpham.tiles4devs.ui.guide.AddTileGuideFragment
 import com.github.simonpham.tiles4devs.ui.guide.RequestPermissionFragment
 import com.github.simonpham.tiles4devs.ui.guide.StartDevelopingFragment
 import com.github.simonpham.tiles4devs.ui.guide.WelcomeFragment
@@ -14,14 +16,12 @@ import com.github.simonpham.tiles4devs.ui.guide.WelcomeFragment
 
 class ViewPagerAdapter internal constructor(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-    private val PAGE_COUNT = 4
-
     override fun getItem(position: Int): Fragment? {
         return when (position) {
-            0 -> WelcomeFragment()
-            1 -> RequestPermissionFragment()
-            2 -> StartDevelopingFragment()
-            3 -> StartDevelopingFragment()
+            PAGE_WELCOME -> WelcomeFragment()
+            PAGE_REQUEST_PERMISSION -> RequestPermissionFragment()
+            PAGE_ADD_TILE_GUIDE -> AddTileGuideFragment()
+            PAGE_START_DEVELOPING -> StartDevelopingFragment()
             else -> throw IllegalArgumentException("Unknown selected fragment $position")
         }
     }
