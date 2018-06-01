20dd7504f1d3dfe21342937e680702ff65cac839
Simon Pham <simonpham.dn@gmail.com>
2018-06-01 14:22:15 +0700

    Button GettingStarted click -> move to next step

--- app/src/main/java/com/github/simonpham/tiles4devs/ui/guide/WelcomeFragment.kt
+++ app/src/main/java/com/github/simonpham/tiles4devs/ui/guide/WelcomeFragment.kt
@@ -2,9 +2,11 @@ package com.github.simonpham.tiles4devs.ui.guide
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.simonpham.tiles4devs.PAGE_REQUEST_PERMISSION
 import com.github.simonpham.tiles4devs.R
 import kotlinx.android.synthetic.main.fragment_welcome.*
 
@@ -23,8 +25,9 @@ class WelcomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val viewPager: ViewPager = activity!!.findViewById(R.id.viewPager) as ViewPager
         btnGettingStarted.setOnClickListener {
-
+            viewPager.currentItem = PAGE_REQUEST_PERMISSION
         }
     }
 }
