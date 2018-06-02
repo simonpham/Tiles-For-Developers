22341cb29eef8163c648e340f8da1e76fd0db4c7
Simon Pham <simonpham.dn@gmail.com>
2018-06-02 16:08:35 +0700

    Init Caffeine tile

---
+++ app/src/main/java/com/github/simonpham/tiles4devs/service/tiles/CaffeineService.kt
@@ -0,0 +1,15 @@
+package com.github.simonpham.tiles4devs.service.tiles
+
+import com.github.simonpham.tiles4devs.service.BaseTileService
+
+/**
+ * Created by Simon Pham on 6/2/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+class CaffeineService : BaseTileService() {
+
+    override fun refresh() {
+        
+    }
+}
