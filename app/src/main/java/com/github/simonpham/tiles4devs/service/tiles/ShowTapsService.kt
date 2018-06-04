caf71b031cf71a1079a9a3eb3d046a3ce0d09f3e
Simon Pham <simonpham.dn@gmail.com>
2018-06-04 11:20:11 +0700

    Init Show taps tile

---
+++ app/src/main/java/com/github/simonpham/tiles4devs/service/tiles/ShowTapsService.kt
@@ -0,0 +1,19 @@
+package com.github.simonpham.tiles4devs.service.tiles
+
+import com.github.simonpham.tiles4devs.service.BaseTileService
+
+/**
+ * Created by Simon Pham on 6/4/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+class ShowTapsService : BaseTileService() {
+
+    override fun refresh() {
+
+    }
+
+    override fun onClick() {
+
+    }
+}
