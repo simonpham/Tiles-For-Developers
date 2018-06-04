8b177957d2532b8f63b26bd3c55317de4f40a6b7
Simon Pham <simonpham.dn@gmail.com>
2018-06-05 00:28:55 +0700

    Add Usb debugging tile service

---
+++ app/src/main/java/com/github/simonpham/tiles4devs/service/tiles/UsbDebuggingService.kt
@@ -0,0 +1,31 @@
+package com.github.simonpham.tiles4devs.service.tiles
+
+import android.service.quicksettings.Tile
+import com.github.simonpham.tiles4devs.SYS_ADB_ENABLED
+import com.github.simonpham.tiles4devs.service.BaseTileService
+
+/**
+ * Created by Simon Pham on 6/5/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+class UsbDebuggingService : BaseTileService() {
+
+    override fun refresh() {
+        qsTile.state = if (isFeatureEnabled()) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
+    }
+
+    override fun onClick() {
+        if (isFeatureEnabled()) {
+            devSettings.setSystemInt(SYS_ADB_ENABLED, 0)
+        } else {
+            devSettings.setSystemInt(SYS_ADB_ENABLED, 1)
+        }
+        refresh()
+    }
+
+    private fun isFeatureEnabled(): Boolean {
+        return devSettings.getSystemInt(SYS_ADB_ENABLED) == 1
+    }
+}
