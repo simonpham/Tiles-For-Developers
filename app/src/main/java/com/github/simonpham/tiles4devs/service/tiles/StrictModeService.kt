097f25fb6193e151511b9e4943cc93284d139fbd
Simon Pham <simonpham.dn@gmail.com>
2018-06-05 00:06:18 +0700

    Init strict mode tile

---
+++ app/src/main/java/com/github/simonpham/tiles4devs/service/tiles/StrictModeService.kt
@@ -0,0 +1,19 @@
+package com.github.simonpham.tiles4devs.service.tiles
+
+import com.github.simonpham.tiles4devs.service.BaseTileService
+
+/**
+ * Created by Simon Pham on 6/5/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+class StrictModeService: BaseTileService() {
+
+    override fun refresh() {
+
+    }
+
+    override fun onClick() {
+
+    }
+}
