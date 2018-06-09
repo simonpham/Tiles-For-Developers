c07aae2dc5ad4969d7e8ef11cf162e2c99a7af31
Simon Pham <simonpham.dn@gmail.com>
2018-06-10 00:23:00 +0700

    Update manifest utils

--- app/src/main/java/com/github/simonpham/devtiles/util/ManifestUtils.kt
+++ app/src/main/java/com/github/simonpham/devtiles/util/ManifestUtils.kt
@@ -1,7 +1,6 @@
 package com.github.simonpham.devtiles.util
 
 import android.content.ComponentName
-import android.content.Context
 import android.content.pm.PackageManager
 import com.github.simonpham.devtiles.SingletonInstances
 
@@ -10,14 +9,23 @@ import com.github.simonpham.devtiles.SingletonInstances
  * Email: simonpham.dn@gmail.com
  */
 
-fun toggleComponent(context: Context, component: Class<Any>, state: Boolean) {
+fun toggleComponent(component: Class<Any>, state: Boolean) {
+    val context = SingletonInstances.getAppContext()
     val componentName = ComponentName(context, component)
     val packageManager = SingletonInstances.getPackageManager()
 
     val newState = if (state) {
         PackageManager.COMPONENT_ENABLED_STATE_ENABLED
     } else {
-        PackageManager.COMPONENT_ENABLED_STATE_DISABLED)
+        PackageManager.COMPONENT_ENABLED_STATE_DISABLED
     }
     packageManager.setComponentEnabledSetting(componentName, newState, PackageManager.DONT_KILL_APP)
+}
+
+fun isComponentEnabled(component: Class<Any>): Boolean {
+    val context = SingletonInstances.getAppContext()
+    val componentName = ComponentName(context, component)
+    val packageManager = SingletonInstances.getPackageManager()
+
+    return packageManager.getComponentEnabledSetting(componentName) == PackageManager.COMPONENT_ENABLED_STATE_ENABLED
 }
