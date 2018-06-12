30c9a4b65fdc7c3ba8f81e374048a5f4c53669bb
Simon Pham <simonpham.dn@gmail.com>
2018-06-12 20:13:42 +0700

    Update strings

--- app/src/main/java/com/github/simonpham/devtiles/Const.kt
+++ app/src/main/java/com/github/simonpham/devtiles/Const.kt
@@ -5,7 +5,7 @@ package com.github.simonpham.devtiles
  * Email: simonpham.dn@gmail.com
  */
 
-// rootless settings
+// sysprop settings
 const val SYSPROP_ADB_PORT = "service.adb.tcp.port"
 const val SYSPROP_DEBUG_FORCE_RTL = "debug.force_rtl"
 const val SYSPROP_DEBUG_GPU_OVERDRAW = "debug.hwui.overdraw"
@@ -14,7 +14,7 @@ const val SYSPROP_DEBUG_LAYOUT = "debug.layout"
 const val SYSPROP_STRICT_MODE_DISABLE = "persist.sys.strictmode.disable"
 const val SYSPROP_STRICT_MODE_VISUAL = "persist.sys.strictmode.visual"
 
-// settings require write secure settings permission
+// settings require Devetter Plugin
 const val GLOBAL_ANIMATOR_DURATION_SCALE = "animator_duration_scale"
 const val GLOBAL_ADB_ENABLED = "adb_enabled"
 const val GLOBAL_DEMO_MODE_ALLOWED = "sysui_demo_allowed"
@@ -23,8 +23,8 @@ const val SYSTEM_SHOW_TAPS = "show_touches"
 
 const val PAGE_COUNT = 4
 const val PAGE_WELCOME = 0
-const val PAGE_REQUEST_PERMISSION = 1
-const val PAGE_ADD_TILE_GUIDE = 2
+const val PAGE_INSTALL_DEVETTER = 1
+const val PAGE_REQUEST_PERMISSION = 2
 const val PAGE_START_DEVELOPING = 3
 
 const val KEY_SU_AVAILABILITY = "KEY.SU_AVAILABILITY"
