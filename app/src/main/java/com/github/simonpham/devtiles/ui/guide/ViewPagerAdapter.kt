8c44533831659d69ed6320ac742e333a26a0f17d
Simon Pham <simonpham.dn@gmail.com>
2018-06-12 20:11:57 +0700

    Add Devetter install board

--- app/src/main/java/com/github/simonpham/devtiles/ui/guide/ViewPagerAdapter.kt
+++ app/src/main/java/com/github/simonpham/devtiles/ui/guide/ViewPagerAdapter.kt
@@ -1,10 +1,7 @@
 package com.github.simonpham.devtiles.ui.guide
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import com.github.simonpham.devtiles.*
-import com.github.simonpham.devtiles.ui.guide.board.AddTileGuideFragment
+import com.github.simonpham.devtiles.ui.guide.board.DevetterPluginFragment
 import com.github.simonpham.devtiles.ui.guide.board.RequestPermissionFragment
 import com.github.simonpham.devtiles.ui.guide.board.StartDevelopingFragment
 import com.github.simonpham.devtiles.ui.guide.board.WelcomeFragment
@@ -19,8 +16,8 @@ class ViewPagerAdapter internal constructor(fm: androidx.fragment.app.FragmentMa
     override fun getItem(position: Int): androidx.fragment.app.Fragment? {
         return when (position) {
             PAGE_WELCOME -> WelcomeFragment()
+            PAGE_INSTALL_DEVETTER -> DevetterPluginFragment()
             PAGE_REQUEST_PERMISSION -> RequestPermissionFragment()
-            PAGE_ADD_TILE_GUIDE -> AddTileGuideFragment()
             PAGE_START_DEVELOPING -> StartDevelopingFragment()
             else -> throw IllegalArgumentException("Unknown selected fragment $position")
         }
